70991275e0833f9bd573d632ecedd0077284e455
Daniele Manassero <daniele.manassero@domino.it>
2025-02-19 17:43:25 +0100

    Forms: edit html template

--- 05-forms/main.js
+++ 05-forms/main.js
@@ -1,4 +1,12 @@
 const formPayment = document.querySelector('#formPayment');
+const amount = document.querySelector('#amount');
+const amountOutput = document.querySelector('#amountVal');
+
+amountOutput.textContent = amount.value;
+
+amount.addEventListener('input', (e) => {
+  amountOutput.textContent = e.target.value;
+});
 
 formPayment.addEventListener('submit', (e) => {
   e.preventDefault();
